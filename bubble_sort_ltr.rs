5b98216ae2cbc4a4a1141062a175e5e8336ba1ec
Mehdi Abdi <m.abdi.public@gmail.com>
2022-10-30 20:19:18 +0330

    add bubble sort

---
+++ bubble_sort_ltr.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut a = [15, 11, 7, 3, 2];
+    bubble_sort(&mut a);
+    println!("{:#?}", a);
+}
+
+fn bubble_sort(input_array: &mut [u32; 5]) {
+    for i in 0..input_array.len() - 1 {
+        for j in (0..input_array.len() - i - 1) {
+            if input_array[j] > input_array[j + 1] {
+                (input_array[j], input_array[j + 1]) = (input_array[j + 1], input_array[j])
+            }
+        }
+    }
+}
